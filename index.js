f8e7b8918c3d5872fee1070a7f12cdd9b7765af5
ws903 <ws903@nyu.edu>
2018-11-18 16:20:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file.
+function lowerCaseDrivers(arr) {
+	return arr.map(function(element) {
+		return element.toLowerCase()
+	})
+}
+
+function nameToAttributes(arr) {
+	return arr.map(function(element) {
+		const userFirst = element.split(' ')[0];
+	    const userLast = element.split(' ')[1];
+
+		return { firstName: userFirst, lastName: userLast }
+	})
+}
+
+function attributesToPhrase(arr) {
+	return arr.map(function(element) {
+		return `${element.name} is from ${element.hometown}`
+	})
+}
